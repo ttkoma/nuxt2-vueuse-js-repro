53bce1d4ba16baf898c5e18bff72675c2ad79a35
Konstantin Lapeev <konstantin.Lapeyev@nexign.com>
2022-02-11 00:32:00 +0300

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# nuxt2-vueuse-js
+
+@vueuse/core repro 'big bundle size'
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# build and run webpack analyzer
+$ yarn stat
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+```
---
+++ components/Test/UseMouse.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <p>Mouse: X: {{ x }}, y:{{ y }}</p>
+</div>
+</template>
+
+<script>
+import {useMouse} from "@vueuse/core";
+
+export default {
+  name: "TestUseMouse",
+  setup() {
+      const { x, y } = useMouse()
+
+      return {
+        x, y
+      }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
